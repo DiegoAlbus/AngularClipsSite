c754c0e5b910b09c0b5e1f53c46b62b872904e88
Diego <52823555+DiegoAlbus@users.noreply.github.com>
2024-10-28 12:49:45 +0100

    Huge security issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,11 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "AIzaSyCT-D7y8AuOV9NwFKsq4LN5qYWjSdTTzoQ",
-    authDomain: "clips-46744.firebaseapp.com",
-    projectId: "clips-46744",
-    storageBucket: "clips-46744.appspot.com",
-    appId: "1:306705170244:web:499c8c7ad9993f2b852111"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxx"
   }
 };
 
